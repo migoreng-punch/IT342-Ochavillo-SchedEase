b32dfbe8a1a8c77717f1f0500136dfa1c451b786
LAB13_USER ON WS07 <L13Y12W07@LabHCI.net>
2026-03-07 11:44:12 +0800

    first commit

---
+++ backend/schedease/src/test/java/edu/cit/ochavillo/schedease/SchedeaseApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.ochavillo.schedease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedeaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
