bd7e4ebd3bf71a998f9533cb97531a850f7fa9e4
migoreng-punch <ooodlesnoodles@proton.me>
2026-03-09 23:38:57 +0800

    feat(exception): added GlobalExceptionHandler

---
+++ backend/schedease/src/main/java/edu/cit/ochavillo/schedease/util/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package edu.cit.ochavillo.schedease.util;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> handleRuntime(RuntimeException ex) {
+        return ResponseEntity
+                .badRequest()
+                .body(Map.of("error", ex.getMessage()));
+    }
+}
