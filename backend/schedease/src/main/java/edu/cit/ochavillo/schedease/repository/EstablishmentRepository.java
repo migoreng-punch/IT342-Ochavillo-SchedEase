a969d704a6f5343d828388f17042486d0586a78b
migoreng-punch <ooodlesnoodles@proton.me>
2026-03-20 01:45:00 +0800

    feat(Establishment): added a prototype function in Establishment Repository

--- backend/schedease/src/main/java/edu/cit/ochavillo/schedease/repository/EstablishmentRepository.java
+++ backend/schedease/src/main/java/edu/cit/ochavillo/schedease/repository/EstablishmentRepository.java
@@ -4,6 +4,7 @@ import edu.cit.ochavillo.schedease.entity.Establishment;
 import edu.cit.ochavillo.schedease.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface EstablishmentRepository extends JpaRepository<Establishment, Long> {
@@ -12,4 +13,5 @@ public interface EstablishmentRepository extends JpaRepository<Establishment, Lo
     Optional<Establishment> findByOwner(User owner);
     boolean existsByName(String name);
     boolean existsByContactEmail(String email);
+    List<Establishment> findByNameContainingIgnoreCase(String name);
 }
